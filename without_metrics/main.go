185388ce63036319659f82eed224794403128b07
kytchett <54973041+kytchett@users.noreply.github.com>
2020-06-19 14:49:31 +0800

    Update main.go

--- without_metrics/main.go
+++ without_metrics/main.go
@@ -10,8 +10,8 @@ import (
 )
 
 func main(){
-	rand.Seed(time.Now().Unix())
-	http.HandleFunc("/abc", index)
+	rand.Seed(time.Now().Unix()) 		// 设置随机数种子
+	http.HandleFunc("/abc", index)		// 普通服务请求处理
 	err := http.ListenAndServe(":5565", nil) // 设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
@@ -20,18 +20,18 @@ func main(){
 
 
 func index(w http.ResponseWriter, r *http.Request) {
-	num:=os.Getenv("Num")
-	if num==""{
-		ans10:=Factorial(10)
+	num:=os.Getenv("Num")			// 获取环境变量
+	if num==""{				// 环境变量无效
+		ans10:=Factorial(10)		// 默认计算10的阶乘
 		str:="there is no env Num. Computed factorial of 10. The answer is "+strconv.Itoa(ans10)+" \n"
 		_,err:=w.Write([]byte(str))
 		if err!=nil{
 			log.Println("err:"+err.Error()+" No\n")
 		}
-	}else{
-		numInt,_:=strconv.Atoi(num)
-		randNum:=rand.Intn(numInt) + 1
-		ans:=Factorial(randNum)
+	}else{					// 环境变量有效
+		numInt,_:=strconv.Atoi(num)	
+		randNum:=rand.Intn(numInt) + 1	// 获取需计算阶乘的随机数，使计算的阶乘大于0
+		ans:=Factorial(randNum)		// 计算阶乘
 		str:="there is env Num. Computed factorial of "+strconv.Itoa(randNum)+". The answer is "+strconv.Itoa(ans)+" \n"
 		_,err:=w.Write([]byte(str))
 		if err!=nil{
@@ -40,7 +40,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func Factorial(n int)int{
+func Factorial(n int)int{		// 计算整数n的阶乘
 	if n<=2{
 		return n
 	}else{
